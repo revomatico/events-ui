a84ec132de798a5a6666c6464cb1d58219e0b4e1
IordacheDario99 <dario.iordache@gmail.com>
2019-07-22 14:44:37 +0300

    implemented add button

--- pages/events.vue
+++ pages/events.vue
@@ -1,83 +1,80 @@
 <template>
-<div>
-  <v-layout row wrap px-0 ma-0 pa-0 fill-height>
-    <v-card width="100%">
-      <v-card-title>
-        Events
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="eventsTable.search"
-          append-icon="search"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="eventsTable.headers"
-        :items="eventsTable.desserts"
-        :search="eventsTable.search"
-        class="elevation-1"
-      >
-        <template v-slot:items="props">
-          <td v-for="i in props.item" :key="i">
-            {{i}}
-          </td>
-        </template>
-      </v-data-table>
-      <v-carousel>
-    <v-carousel-item
-      v-for="(item,i) in eventsTable.desserts"
-      :key="i"
-      :src="item[4]"
-    ></v-carousel-item>
-  </v-carousel>
-    </v-card>
-  </v-layout>
-  <v-layout row wrap>
-    <v-flex xs12 sm6>
-      <v-date-picker
-        v-model="dates"
-        multiple
-      ></v-date-picker>
-    </v-flex>
-    <v-flex xs12 sm6>
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        :return-value.sync="dates"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-combobox
-            v-model="dates"
-            multiple
-            chips
-            small-chips
-            label="Multiple picker in menu"
-            prepend-icon="event"
-            readonly
-            v-on="on"
-          ></v-combobox>
-        </template>
-        <v-date-picker v-model="dates" multiple no-title scrollable>
+  <div>
+    <v-layout row wrap px-0 ma-0 pa-0 fill-height>
+      <v-card width="100%">
+        <v-card-title>
+          Events
+          <v-flex>
+            <popup />
+          </v-flex>
           <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-          <v-btn flat color="primary" @click="$refs.menu.save(dates)">OK</v-btn>
-        </v-date-picker>
-      </v-menu>
-    </v-flex>
-  </v-layout>
+          <v-text-field
+            v-model="eventsTable.search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="eventsTable.headers"
+          :items="eventsTable.desserts"
+          :search="eventsTable.search"
+          class="elevation-1"
+        >
+          <template v-slot:items="props">
+            <td v-for="i in props.item" :key="i">{{i}}</td>
+          </template>
+        </v-data-table>
+        <v-carousel>
+          <v-carousel-item v-for="(item,i) in eventsTable.desserts" :key="i" :src="item[4]"></v-carousel-item>
+        </v-carousel>
+      </v-card>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-date-picker v-model="dates" multiple></v-date-picker>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          :return-value.sync="dates"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-combobox
+              v-model="dates"
+              multiple
+              chips
+              small-chips
+              label="Multiple picker in menu"
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-combobox>
+          </template>
+          <v-date-picker v-model="dates" multiple no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.menu.save(dates)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
+
 <script>
+import popup from './popup'
 export default {
+  components: { popup },
   data() {
     return {
       eventsTable: {
@@ -85,13 +82,12 @@ export default {
         headers: [
           { text: 'Name', value: '[0]' },
           { text: 'Date', value: '[1]' },
-          { text: 'Singers', value: '[2]' }
-          
+          { text: 'Singers', value: '[2]' },
         ],
-        desserts: []
+        desserts: [],
       },
-      dates: ['2018-09-15','2018-09-20'],
-      menu: false
+      dates: [],
+      menu: false,
     }
   },
   mounted() {
@@ -104,12 +100,12 @@ export default {
         .then(out => {
           this.eventsTable.desserts = out
           //console.log('all events are loaded: ', out)
-          setTimeout(this.readAll,1000)
+          setTimeout(this.readAll, 1000)
         })
         .catch(err => {
           throw err
         })
-    }
-  }
+    },
+  },
 }
 </script>
---
+++ pages/popup.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog max-width="800px">
+    <v-btn slot="activator" small dark color="#737373">
+      <v-icon>add</v-icon>
+      <span small>Add New Events</span>
+    </v-btn>
+    <v-card>
+      <v-card-title>
+        <template>
+          <v-expansion-panel>
+            <v-expansion-panel-content v-for="(item,i) in 1" :key="i">
+              <template v-slot:header>
+                <div>New Concert</div>
+              </template>
+              <v-card-text>
+                  <v-form class="px-3">
+                      <v-text-field label="Concert" v-model="concert" prepend-icon="music_note"></v-text-field>
+                      <v-text-field label="Date" v-model="date" prepend-icon="event"></v-text-field>
+                      <v-text-field label="Singer" v-model="singer" prepend-icon="record_voice_over"></v-text-field>
+                      <v-btn class="success mx-0 mt-3" small dark >Add Event</v-btn>
+                  </v-form>
+              </v-card-text>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </template>
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+        </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            concert: '',
+            date: '',
+            singer: '',
+        }
+    }
+}
+</script>
+
