865971d900d0ee68ec7d2625cad2f7967bac906c
Dan <51034292+Danutz99@users.noreply.github.com>
2019-07-24 02:18:37 +0300

    Updating footer by adding some social networks buttons
    To make this one work,you must add:<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet"> into the <head></head> of index.html or app.template.html

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-container id="footer" p-0 text-center>
-    © 2019 revomatico. – All rights reserved.
-  </v-container>
+  <v-footer
+  absolute
+  app
+  height="auto"
+  color="#f9fbe7"
+    
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+ <v-btn>
+          <a href="https://www.facebook.com">
+          <v-icon size="24px" color="blue">fab fa-facebook</v-icon></a>
+        </v-btn>
+        <v-btn>
+          <a href="https://twitter.com" color="blue">
+          <v-icon size="24px">fab fa-twitter</v-icon></a>
+        </v-btn>
+      <v-btn>
+          <a href="https://linkedin.com">
+          <v-icon size="24px" color="blue">fab fa-linkedin</v-icon></a>
+        </v-btn>
+        <v-btn>
+          <a href="https://www.instagram.com">
+          <v-icon size="24px" color="red darken-1">fab fa-instagram</v-icon></a>
+        </v-btn>
+      </v-card-title>
+      <v-card-actions class="grey darken-3 justify-center">
+     © 2019 revomatico. – All rights reserved.
+      </v-card-actions>
+    </v-card>
+  </v-footer>
 </template>
-
 <script>
-
-export default {
-  name: 'appFooter',
-  data() {
-    return {
-    }
-  },
-  methods: {}
-}
+  export default {
+  }
 </script>
